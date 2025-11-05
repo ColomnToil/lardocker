6545e8077a4fb27a1f05102b3ea78e8b12b2aaaa
ColomnToil <colomntoil12@mail.ru>
2025-11-05 19:42:45 +0300

    Урок 3-4: добавили фронт страницу, добавили админ панель

--- laravel-docker/routes/web.php
+++ laravel-docker/routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
+use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', [IndexController::class, '__invoke'])->name('main.index');
+});
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', [AdminIndexController::class, '__invoke'])->name('main.index');
+    });
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
